ed10b093559945f4e63a1ce4defaeb69806c64c4
Vodca <122147672+VodcaSmith@users.noreply.github.com>
2024-05-16 08:39:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "token": "MTIzNzIwODU2MDg5OTg1NDUyOQ.GKoyv5.mRSqZIyU5I7YdgymjwTg34kI2zDeSH4bp8hXIc",
-    "prefix": "-",
-    "fakeOnline": 0,
-    "ownerID": "805499912900182117",
-    "statusChannelID": "1171269654824620093",
-    "userReportWebhook": "https://discord.com/api/webhooks/1223561315298054165/82ApDme7I7voEEBluAuBe8lyXi-E-ONRVwiNFY4U5-UfBIjB_jUmgnte4jfCUFT4wpRv",
-    "appName": "dnplayer",
-    "appLocation": "C:\\LDPlayer\\LDPlayer9",
-    "fileLocation": "C:\\Users\\rizky\\OneDrive\\Documents\\Server Test Bot\\database\\online.txt",
-    "folderPlayers": "C:\\Users\\rizky\\OneDrive\\Documents\\Server Test Bot\\players",
-    "folderWorlds": "C:\\Users\\rizky\\OneDrive\\Documents\\Server Test Bot\\worlds",
-    "folderGuilds": "C:\\Users\\rizky\\OneDrive\\Documents\\Server Test Bot\\guilds"
+    "token": "", //BOT TOKEN
+    "prefix": "-", //CHANGE PREFIX HERE
+    "fakeOnline": 0, //DONT CHANGE THIS ONE
+    "ownerID": "", //COPY YOUR USER ID FOR ACCESS FAKE ONLINE
+    "statusChannelID": "", //FOR STATS CHANNEL
+    "userReportWebhook": "", //IF YOU REPORT BUG OR SOMETHING I WILL REPLY YOUR MESSAGE ON THIS WEBHOOK
+    "appName": "", //EXE NAME, EXAMPLE GTPS.EXE / GTPS
+    "appLocation": "C:\\", //LOCATION EXE / APPNAME
+    "fileLocation": "C:\\Users\\online.txt", //LOCATION ONLINE TXT
+    "folderPlayers": "C:\\Users\\players", //LOCATION PLAYERS FOLDER
+    "folderWorlds": "C:\\Users\\worlds", //LOCATION WORLDS FOLDER
+    "folderGuilds": "C:\\Users\\guilds" //LOCATION GUILDS FOLDER
 };
