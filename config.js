bb6fedaf94e12cbf1978cda815951fea2094e942
Vodca <122147672+VodcaSmith@users.noreply.github.com>
2024-05-20 07:43:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,4 +11,18 @@ module.exports = {
     "folderPlayers": "C:\\Users\\players", //LOCATION PLAYERS FOLDER
     "folderWorlds": "C:\\Users\\worlds", //LOCATION WORLDS FOLDER
     "folderGuilds": "C:\\Users\\guilds" //LOCATION GUILDS FOLDER
+
+     //LockID / ItemID Lock for command "check and panel"
+    "WLID": "242",
+    "DLID": "1796",
+    "BGLID": "7188",
+    "GGLID": "8470",
+
+    //Copy Your Lock Emoji for command "check and panel"
+    "OPCE": "<:opc:1227488387955687434>",
+    "GEMSE": "<:gems:1177551577628811334>",
+    "WLE": "<a:wlv2:1185773232108867656>",
+    "DLE": "<a:dlv2:1184187703089172663>",
+    "BGLE": "<a:bglv2:1184187824556224572>",
+    "GGLE": "<:ggl:1200747876406534154>"
 };
